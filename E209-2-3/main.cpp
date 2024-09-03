01e2ce1db377ab43cea5563b948592cee801dfea
Irineu <irineo.rocha@gmail.com>
2024-09-03 13:02:41 -0300

    coloca em funcao

--- E209-2-3/main.cpp
+++ E209-2-3/main.cpp
@@ -3,28 +3,28 @@
 //cada uma das n vezes
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main()
+void quant(int n)
 {
-    int n; //quant de testes
     int m;
+    vector<int> v1;
     int neg = 0;
     int pos = 0;
     int par = 0;
     int imp = 0;
-    vector<int> v1;
-    
-    cin >> n;
+
     for(int i=0;i<n;i++)
     {
         for(int i=1;i<=5;i++)
         {
             cin >> m;
-            v1.push_back(m);        
+            v1.push_back(m);
         }
     }
+
     for (auto i = v1.begin(); i != v1.end(); ++i)
     {
         if(*i < 0)
@@ -34,11 +34,18 @@ int main()
             par++;
         else imp++;
     }
-    //cout << *i << " ";
+
     cout << "Quantidade de numeros pares: " << par << endl;
     cout << "Quantidade de numeros impares: " << imp << endl;
     cout << "Quantidade de numeros positivos: " << pos << endl;
     cout << "Quantidade de numeros negativos: " << neg << endl;
+}
 
-    
+int main()
+{
+    int n; //quant de testes
+
+    cin >> n;
+    quant(n);
+
 }
