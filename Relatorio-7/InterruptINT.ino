54b141be2f34f1e30ace30575caa0f656f377b24
Irineu <irineo.rocha@gmail.com>
2024-09-20 21:12:17 -0300

    Update InterruptINT.ino

--- Relatorio-7/InterruptINT.ino
+++ Relatorio-7/InterruptINT.ino
@@ -1,23 +1,21 @@
 // Na interrupção PCINT0
-ISR(PCINT0_vect)
+ISR(PCINT0_vect) //PD7
 {
+if(PINB & 0b00000001){
 /* CÓDIGO QUE VAI RODAR
 NA INTERRUPÇÃO DO PCINT0 */
 PORTD ^= 0b10000000;
 _delay_ms(1000);
 PORTD ^= 0b10000000;
 }
-// Na interrupção PCINT1
-ISR(PCINT1_vect)
-{
-/* CÓDIGO QUE VAI RODAR
-NA INTERRUPÇÃO DO PCINT1 */
+if(PINB & 0b00000100){
 PORTD ^= 0b01000000;
 _delay_ms(500);
 PORTD ^= 0b01000000;
 }
+}
 // Na interrupção PCINT2
-ISR(PCINT2_vect)
+ISR(PCINT2_vect) //PB1
 {
 /* CÓDIGO QUE VAI RODAR
 NA INTERRUPÇÃO DO PCINT2 */
@@ -27,14 +25,12 @@ PORTB ^= 0b00000010;
 }
 int main(void)
 {
-DDRD = 0b11100000;
-DDRB = 0b00000010;
-PORTD = 0b00010110;
-// Habilita interrupção no grupo PCINT2 & INT0
-PCICR |= 0b00000111;
-// Habilita PCINT no pino PD4 -> PCINT20
-PCMSK0 |= 0b00000100;
-PCMSK1 |= 0b00000010;
+DDRD = 0b11100000; //PD5 PD6 PD7 COMO SAIDA
+DDRB = 0b00000010; //PB1 COMO SAIDA
+// Habilita interrupção no grupo PCINT2 INT1 & INT0
+PCICR |= 0b00000101;
+// Habilita PCINT no pino PB0 -> PCINT0 PB2 -> PCINT2 PD4 -> PCINT20
+PCMSK0 |= 0b00000101;
 PCMSK2 |= 0b00010000;
 // Habilita Interrupções globais
 sei();
